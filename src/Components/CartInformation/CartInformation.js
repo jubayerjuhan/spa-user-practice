1ca8f23a86c8f9be672139e2fb6046c1ae2dbc1c
jubayerjuhan <davidjuhan23@gmail.com>
2021-07-04 16:16:18 +0600

    complete

--- src/Components/CartInformation/CartInformation.js
+++ src/Components/CartInformation/CartInformation.js
@@ -9,14 +9,15 @@ const CartInformation = (props) => {
     for (let i = 0; i < user.length; i++) {
         const people = user[i];
         totalIncome = totalIncome + people.income ;
-        
     }
+    
+    totalIncome = totalIncome.toFixed(2)
     return (
         <div className="information">
             <div>
                 <h2>This Is Information</h2>
-                <p>Total Added: {user.length}</p>
-                <p>{user.length} Peoples Total Yearly Income {totalIncome}</p>
+                <p><code><small>Total Added: {user.length}</small></code></p>
+                <p><code><small>{user.length} Peoples Total Yearly Income ${totalIncome}</small></code></p>
             </div>
         </div>
     );
